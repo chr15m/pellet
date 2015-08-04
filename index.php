f18920906ce688ae62be7cbd3361d7a715163d54
Chris McCormick <chris@mccormick.cx>
2015-08-04 21:29:55 +0800

    Ability to request raw result when using proxy.

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ function proxy() {
     $response = file_get_contents($_REQUEST["proxy"]);
     json_decode($response);
     // if the destination returns json then don't double encode
-    if (strpos($_SERVER['QUERY_STRING'], "json") !== false && json_last_error() == JSON_ERROR_NONE) {
+    if (isset($_REQUEST["raw"]) || (strpos($_SERVER['QUERY_STRING'], "json") !== false && json_last_error() == JSON_ERROR_NONE)) {
       die($response);
     } else {
       die(json_encode($response));
